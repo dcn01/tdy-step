3bf2421ceda887a6e5f2bf8765465843336892b1
cxm <1690364680@qq.com>
2020-01-16 08:33:55 +0800

    add 灭霸

--- lib/pages/snap_page.dart
+++ lib/pages/snap_page.dart
@@ -10,15 +10,13 @@ class _SnapPageState extends State<SnapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Snap(
-            child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.yellow,
-          child: Text('将html转成canvas，为后面处理做准备。这里我们使用html2canvas库，可以将html中的dom节点绘制成canvas'),
-        )),
-      ),
+      body: Snap(
+          child: Image.network(
+        'https://norefer.oss.dcdcdog.com/12120.jpg',
+        fit:BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      )),
     );
   }
 }
--- lib/utils/snap.dart
+++ lib/utils/snap.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_animation_set/animation_set.dart';
+import 'package:flutter_animation_set/animator.dart';
 import 'package:image/image.dart' as image;
 
 class Snap extends StatefulWidget {
@@ -13,60 +16,74 @@ class Snap extends StatefulWidget {
 }
 
 class _SnapState extends State<Snap> {
-  var size, images, imgs = [], gKey = GlobalKey();
+  var imgs = [], gKey = GlobalKey();
 
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(milliseconds: 100), getImgFromWidget);
+    // 延迟3秒,等图片加载好
+    Future.delayed(Duration(seconds: 3), snap);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: imgs.length == 0
-          ? [
-              RepaintBoundary(
-                key: gKey,
-                child: widget.child,
-              )
-            ]
-          : imgs
-              .map<Widget>((item) => Padding(
-                    padding: EdgeInsets.all(10),
+    return Stack(children: [
+      if (imgs.length == 0)
+        RepaintBoundary(
+          key: gKey,
+          child: widget.child,
+        ),
+      if (imgs.length != 0) ...[
+        // 加这个挡下,不然会白屏一下
+        AnimatorSet(
+            animationType: AnimationType.once,
+            child: widget.child,
+            animatorSet: [
+              Serial(
+                  duration: 3000,
+                  serialList: [O(from: 1, to: 0, curve: Curves.linear)]),
+            ]),
+        ...imgs
+            .map<Widget>((item) => AnimatorSet(
+                    animationType: AnimationType.once,
                     child: Image.memory(item),
-                  ))
-              .toList(),
-    );
+                    animatorSet: [
+                      Serial(
+                          duration: Random().nextInt(6000) + 1000,
+                          serialList: [
+                            TY(
+                                from: 0,
+                                to: -200.0 - Random().nextInt(200),
+                                curve: Curves.easeInCirc),
+                            TX(
+                                from: 0,
+                                to: 200.0 + Random().nextInt(200),
+                                curve: Curves.easeInCirc),
+                            O(from: 1, to: 0, curve: Curves.linear)
+                          ]),
+                    ]))
+            .toList()
+      ]
+    ]);
   }
 
-  getImgFromWidget() async {
+  snap() async {
     RenderRepaintBoundary boundry = gKey.currentContext.findRenderObject();
-    debugPrint('start----${DateTime.now().second}.${DateTime.now().millisecond}');
-    size = boundry.size;
-    var img = await boundry.toImage();
-    var byteData = await img.toByteData(format: ImageByteFormat.rawRgba);
-    var pngBytes = byteData.buffer.asUint8List();
-    var copy = List.filled(pngBytes.length, 0, growable: true);
-    var copy1 = List.filled(pngBytes.length, 0, growable: true);
-    var copy2 = List.filled(pngBytes.length, 0, growable: true);
-    copy.replaceRange(0, 10000, pngBytes.sublist(0, 10000));
-    copy1.replaceRange(10000, 20000, pngBytes.sublist(10000, 20000));
-    copy2.replaceRange(20000, 40000, pngBytes.sublist(20000, 40000));
-    // debugPrint('pngBytes:$pngBytes');
-    debugPrint('pngBytes length:${pngBytes.lengthInBytes}');
-    // var length = pngBytes.lengthInBytes;
-    // debugPrint(
-    //     'length:$length---${byteData.buffer.asUint8ClampedList().length}');
-    imgs = [
-      image.encodePng(image.Image.fromBytes(img.width, img.height, copy)),
-      image.encodePng(image.Image.fromBytes(img.width, img.height, copy1)),
-      image.encodePng(image.Image.fromBytes(img.width, img.height, copy2)),
-    ];
+    debugPrint(
+        'start----${DateTime.now().second}.${DateTime.now().millisecond}');
+    var fullImg = await boundry.toImage();
+    var byteData = await fullImg.toByteData(format: ImageByteFormat.rawRgba);
+    var rgbData = byteData.buffer.asUint8List();
+    var partData = List.generate(
+        30, (i) => List.filled(rgbData.length, 0, growable: true));
+    for (int i = 0; i < rgbData.length; i += 4) {
+      var randomIndex = Random().nextInt(10);
+      partData[randomIndex].replaceRange(i, i + 4, rgbData.sublist(i, i + 4));
+    }
+    partData.forEach((item) {
+      imgs.add(image.encodePng(
+          image.Image.fromBytes(fullImg.width, fullImg.height, item)));
+    });
     setState(() {});
-    debugPrint('end----${DateTime.now().second}.${DateTime.now().millisecond}');
-    // debugPrint('pngBytes:$pngBytes');
-    // debugPrint('image.decodeImage(pngBytes):${image.decodeImage(pngBytes)}');
-    // return image.decodeImage(pngBytes);
   }
 }
